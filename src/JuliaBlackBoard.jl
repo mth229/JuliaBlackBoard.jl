62debf0a2128195a51009ea8abad0fce41915694
jverzani <jverzani@gmail.com>
2020-09-25 10:58:41 -0400

    use preview; use sips on apple

--- src/JuliaBlackBoard.jl
+++ src/JuliaBlackBoard.jl
@@ -4,6 +4,12 @@ using Markdown
 using Mustache
 using Base64
 
+# for LaTeX
+using Tectonic
+using FileIO
+using ImageMagick
+
+
 include("markdown-additions.jl")
 include("questions.jl")
 
--- src/markdown-additions.jl
+++ src/markdown-additions.jl
@@ -25,10 +25,6 @@ function File(p)
 end
 export File
 
-using Mustache
-using Tectonic
-using ImageMagick
-
 const WHITE = ImageMagick.RGBA{ImageMagick.N0f16}(1.0,1.0,1.0,0.0)
 
 const latex_tpl = mt"""
@@ -47,8 +43,9 @@ const latex_tpl = mt"""
 \end{document}
 """
 
-const standalone = mt"""
-\documentclass{standalone}
+
+const preview_tpl = mt"""
+\documentclass[24pt]{article}
 \usepackage{amssymb}
 \usepackage{graphicx}
 \usepackage{tikz}
@@ -56,16 +53,37 @@ const standalone = mt"""
 \usepackage{ {{.}} }
 {{/:pkgs}}
 
+\usepackage[active, tightpage]{preview}
+\setlength\PreviewBorder{10pt}%
+
 \begin{document}
+\thispagestyle{empty}
+\begin{preview}
 \{{:fontsize}}
 {{{:txt}}}
+\end{preview}
 \end{document}
 """
 
-function latex_to_image(str;fontsize="LARGE", tpl=latex_tpl, pkgs=[])
+
+function _findfirst(rs)
+    i = findfirst(iszero, rs)
+    i == nothing ? 1 : i
+end
+
+function _findlast(rs)
+    i = findlast(iszero, rs)
+    i == nothing ? length(rs) : i
+end
+
+# convert to png file
+function latex_to_image(str;  fontsize="LARGE", tpl=preview_tpl, pkgs=[])
 
     fnm = tempname()
     fnmtex = fnm * ".tex"
+    fnmpdf = fnm * ".pdf"
+    fnmpng = fnm * ".png"
+    
     open(fnmtex, "w") do io
         Mustache.render(io, tpl, (txt=str, fontsize=fontsize, pkgs=collect(pkgs)))
     end
@@ -73,30 +91,40 @@ function latex_to_image(str;fontsize="LARGE", tpl=latex_tpl, pkgs=[])
         run(`$bin $fnmtex`)
     end
 
+    run(`cp $fnmtex /tmp/test.tex`)
 
-    a = ImageMagick.load(fnm * ".pdf")
+    if Sys.isapple()
+        run(`sips -s format png $fnmpdf --out $fnmpng`)
+        return fnmpng
+    end
     
-    rs = [all(a[i,:] .== WHITE) for i in 1:size(a, 1)]
-    cs = [all(a[:,j] .== WHITE) for j in 1:size(a, 2)]
-    rₘ = findfirst(iszero, rs)
-    rₙ = findlast(iszero, rs)
-    cₘ = findfirst(iszero, cs)
-    cₙ = findlast(iszero, cs)
-    b = a[rₘ:rₙ, cₘ:cₙ]
-
-    fnmpng = tempname() * ".png"
-    ImageMagick.save(fnmpng, b)
-
+    a = load(fnmpdf)
+    save(fnmpng, a, quality=100)
     return fnmpng
+
+    # Keep in case useful elsewhere; no longer used as with preview no need to trim
+    # hack to trim pdf and write out png
+    # rs = [all(a[i,:] .== WHITE) for i in 1:size(a, 1)]
+    # cs = [all(a[:,j] .== WHITE) for j in 1:size(a, 2)]
+    # rₘ = _findfirst(rs)
+    # rₙ = _findlast(rs)
+    # cₘ = _findfirst(cs)
+    # cₙ = _findlast(cs)
+    # b = a[rₘ:rₙ, cₘ:cₙ]
+
+    # save(fnmpng, b, quality=100)
+    # return fnmpng
 end
 
+
 # show the png file generated
-function preview(str; tpl=standalone, fontsize="LARGE", pkgs=[])
+function preview(str; tpl=preview_tpl, fontsize="LARGE", pkgs=[])
     fnm = tempname()
     fnmtex = fnm * ".tex"
     open(fnmtex, "w") do io
         Mustache.render(io, tpl, (txt=str, fontsize=fontsize, pkgs=collect(pkgs)))
     end
+    run(`cp $fnmtex /tmp/str.tex`)
     tectonic() do bin
         run(`$bin $fnmtex`)
     end
@@ -104,8 +132,18 @@ function preview(str; tpl=standalone, fontsize="LARGE", pkgs=[])
 
 end
 
-function LaTeX(str; fontsize="LARGE", tpl=latex_tpl, pkgs=[])
+# go from LaTeX snippet to a png file
+# very lossy unless `standalone` is used
+function LaTeX(str, context=nothing;
+               tpl=preview_tpl,
+               fontsize="LARGE",  pkgs=[])
+
+    if !isnothing(context)
+        str = Mustache.render(str, context)
+    end
+
     fnm = latex_to_image(str, fontsize=fontsize, tpl=tpl, pkgs=pkgs)
+
     img = base64encode(read(fnm, String))
     io = IOBuffer()
     print(io,"data:image/gif;base64,")
@@ -113,29 +151,26 @@ function LaTeX(str; fontsize="LARGE", tpl=latex_tpl, pkgs=[])
     String(take!(io))
 end
 
-function LaTeX(_tpl, context; fontsize="LARGE", tpl=latex_tpl, pkgs=[])
-    str = Mustache.render(_tpl, context)
-    LaTeX(str; fontsize=fontsize, tpl=tpl, pkgs=pkgs)
-end
 
-
-
-export LaTeX, preview
+export pdflatex, LaTeX, preview
 
 
 function Base.show(io::IO, ::MIME"text/html", md::Markdown.Image)
-
+    # img not object
     println(io, """<figure><img src="$(md.url)"  alt="$(md.alt)"><figcaption>$(md.alt)</figcaption></figure>""")
 
 end
 
-# create HTML
+# create HTML for question and context
 function create_html(q, context; strip=false)
-    qq =  sprint(io -> show(io, "text/html", Markdown.parse(Mustache.render(q, context))))
+
+    str = Mustache.render(q, context)
+    qq =  sprint(io -> show(io, "text/html", Markdown.parse(str)))
     qq = replace(qq, "\n" => "")
     qq = qq[23:end-6]
     if strip
         qq = qq[4:end-5]
     end
     qq
+
 end
