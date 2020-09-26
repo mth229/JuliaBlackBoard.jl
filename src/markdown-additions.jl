aeb391d6ff808df9f9a4e5d353c4ddcd46b8895a
jverzani <jverzani@gmail.com>
2020-09-26 08:02:23 -0400

    add tth

--- src/markdown-additions.jl
+++ src/markdown-additions.jl
@@ -30,8 +30,6 @@ const WHITE = ImageMagick.RGBA{ImageMagick.N0f16}(1.0,1.0,1.0,0.0)
 const latex_tpl = mt"""
 \documentclass{article}
 \usepackage{amssymb}
-\usepackage{graphicx}
-\usepackage{tikz}
 {{#:pkgs}}
 \usepackage{ {{.}} }
 {{/:pkgs}}
@@ -174,3 +172,48 @@ function create_html(q, context; strip=false)
     qq
 
 end
+
+# this didn't work either :(
+# they use an img and a link to a generated graphic.
+function create_mathml(str, context=nothing)
+
+    ltx = !(context == nothing) ?  Mustache.render(str, context) : str
+    #mml = latex_to_mathml(ltx)
+    mml = """
+<math xmlns="http://www.w3.org/1998/Math/MathML">
+<mrow>
+<msup>
+<mi>a</mi>
+<mi>b</mi>
+</msup>
+</mrow>
+</math>
+"""
+
+    mml = replace(mml, "\n" => "")
+    
+
+end
+
+
+function latex_to_html(str, context=nothing)
+
+    ltx = !(context == nothing) ?  Mustache.render(str, context) : str
+
+    tthbinary = "tth" # XXX
+
+    fnm = tempname()
+    _tex =  fnm * ".tex"
+    _html = fnm * ".html"
+    open(_tex, "w") do io
+        Mustache.render(io, latex_tpl, (fontsize="large", pkgs=[], txt=ltx))
+    end
+
+    io = IOBuffer()
+    run(pipeline(`tth -g -i -r -w`, stdin=_tex, stdout=io))#, stdout=_html))
+    out = String(take!(io))
+    out
+
+end
+    
+    
